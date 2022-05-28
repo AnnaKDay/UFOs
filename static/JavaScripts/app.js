c4fd8a80bfe7f73a321db5e5b7aafeef2d48d838
AnnaKDay <katsukoday@gmail.com>
2022-05-28 10:31:00 -0500

    built functions

--- static/JavaScripts/app.js
+++ static/JavaScripts/app.js
@@ -22,3 +22,23 @@ data.forEach((dataRow) => {
         cell.text(val);
     });
 });
+
+// click function
+function handleClick() {
+    // grab the datetime value from the filter
+    let date = d3.select('#datetime').property('value');
+    let filteredData = tableData;
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    // rebuild the table using the filtered data
+    // @NOTE: if no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+};
+
+// Attach an event to listen for the form button
+d3.selectAll("filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
